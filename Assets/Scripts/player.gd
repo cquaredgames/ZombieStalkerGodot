31aa4279ee5befe0641de1c4faeab0f1fb65fea1
csquaredtimesinfinity <csquaredgames3@gmail.com>
2025-09-17 14:22:08 -0500

    latest update

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -6,10 +6,15 @@ class_name PlayerController
 
 enum Direction { LEFT, RIGHT, UP, DOWN }
 var player_direction = Direction.RIGHT
+var space_pressed = false
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
-	
+	if Input.is_key_pressed(KEY_SPACE) && !space_pressed:
+		AudioManager.play("res://Assets/sound_effects/balloon_pop.wav")
+		space_pressed = true
+	else:
+		space_pressed = false
 	if Input.is_action_pressed("move_up"):
 		input_vector.y -= 1
 		player_direction = Direction.UP
